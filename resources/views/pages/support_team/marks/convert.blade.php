de9442871bcb336cb1292cb216bddabfff5503a3
rayusamBoy <rysmtulia07@gmail.com>
2024-10-29 14:16:02 +0300

    Update: add margin to the right

--- resources/views/pages/support_team/marks/convert.blade.php
+++ resources/views/pages/support_team/marks/convert.blade.php
@@ -26,7 +26,7 @@
                         <i class="material-symbols-rounded float-right text-info display-none" data-toggle="tooltip" title="The total score, out of which the current scrore is based (old score demoninator).">info</i>
                     </label>
                     <input class="form-control border-gray-500" placeholder="Eg., 60" type="number" min="0" max="100" id="from-out-of">
-                    <div class="invalid-feedback font-size-md"><i class="material-symbols-rounded">block</i><span></span></div>
+                    <div class="invalid-feedback font-size-md"><i class="material-symbols-rounded mr-1">block</i><span></span></div>
                 </div>
             </div>
 
@@ -36,7 +36,7 @@
                         <i class="material-symbols-rounded float-right text-info display-none" data-toggle="tooltip" title="The overall score, on which the new score should based (new denominator)">info</i>
                     </label>
                     <input class="form-control" placeholder="Eg., 100" type="number" min="0" max="100" id="to-out-of">
-                    <div class="invalid-feedback font-size-md"><i class="material-symbols-rounded">block</i><span></span></div>
+                    <div class="invalid-feedback font-size-md"><i class="material-symbols-rounded mr-1">block</i><span></span></div>
                 </div>
             </div>
         </div>
@@ -56,7 +56,7 @@
                 {{-- <i class="material-symbols-rounded cursor-pointer position-absolute right-2 opacity-50 opacity-100-on-hover transition-all display-none" title="remove item" onclick="$(this).parent().remove();">close</i> --}}
                 <div class="col-8 col-sm-5">
                     <input class="form-control item-input" placeholder="Enter a Mark" type="number">
-                    <div class="invalid-feedback font-size-md"><i class="material-symbols-rounded">block</i><span></span></div>
+                    <div class="invalid-feedback font-size-md"><i class="material-symbols-rounded mr-1">block</i><span></span></div>
                 </div>
                 <div class="col-2 col-sm-2 material-symbols-rounded m-auto">line_end_arrow</div>
 
