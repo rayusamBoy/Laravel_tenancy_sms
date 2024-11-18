21fbf82543bc3f3da0edc45d40037fd151aad92c
rayusamBoy <rysmtulia07@gmail.com>
2024-11-18 09:49:28 +0300

    Randomize easing and update anime.js dependency

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -14,6 +14,7 @@
             height: 5vw;
             margin: auto
         }
+
     </style>
 
     @stack('css')
@@ -23,14 +24,14 @@
     @include('errors.inc_top')
 </head>
 
-<body>   
+<body>
     <div class="navbar navbar-dark login">
         @if(session('tenant_account_not_active'))
         <h6 class="text-bold text-info text-color-custom mt-auto mb-auto">{{ session('tenant_account_not_active') }}</h6>
         @else
         <h6 class="mt-auto mb-auto text-color-custom">@yield('title')</h6>
         @endif
-    
+
         <a href="{{ route('home') }}" class="navbar-nav-link d-flex text-color-custom float-right">
             <span class="mr-1">{{ ("Home") }}</span>
             <i class="material-symbols-rounded">home</i>
@@ -46,7 +47,7 @@
         anime({
             targets: 'svg path',
             strokeDashoffset: [anime.setDashoffset, 0],
-            easing: 'easeInOutSine',
+            easing: getRandomEasing(),
             duration: 1500,
             delay: function(el, i) {
                 return i * 250
@@ -54,6 +55,33 @@
             direction: 'alternate',
             loop: true
         });
+
+        function getRandomEasing() {
+            var easings = [
+                'easeInBack', 'easeInBounce', 'easeInCirc', 'easeInCubic', 'easeInElastic(' + getRandomNumberBtnTwoInclusive(1, 10) + ',' + getRandomNumberBtnTwoInclusive(0.2, 2) + ')',
+                'easeOutElastic(' + getRandomNumberBtnTwoInclusive(1, 10) + ',' + getRandomNumberBtnTwoInclusive(0.2, 2) + ')', 'easeInOutElastic(' + getRandomNumberBtnTwoInclusive(1, 10) + ',' + getRandomNumberBtnTwoInclusive(0.2, 2) + ')', 'easeOutInElastic(' + getRandomNumberBtnTwoInclusive(1, 10) + ',' + getRandomNumberBtnTwoInclusive(0.2, 2) + ')',
+                'easeInElastic(' + getRandomNumberBtnTwoInclusive(1, 10) + ',' + getRandomNumberBtnTwoInclusive(0.2, 2) + ')', 'easeInExpo', 'easeInOutBack', 'easeInOutBounce', 'easeInOutCirc',
+                'easeInOutCubic', 'easeInOutExpo', 'easeInOutQuint', 'easeInOutQuart', 'easeInOutSine',
+                'easeInOutQuad', 'easeInQuad', 'easeInQuint', 'easeInSine', 'easeInExpo',
+                'easeOutBack', 'easeOutBounce', 'easeOutCirc', 'easeOutCubic', 'easeOutExpo',
+                'easeOutInBack', 'easeOutInBounce', 'easeOutInCirc', 'easeOutInCubic', 'easeOutInExpo',
+                'easeOutInQuad', 'easeOutInQuart', 'easeOutInSine', 'easeOutInQuint', 'easeOutQuad',
+                'easeOutQuart', 'easeOutSine', 'easeOutQuint', 'linear', 'steps(' + getRandomNumber() + ')',
+                'spring(' + getRandomNumberBtnTwoInclusive(0, 100) + ', ' + getRandomNumberBtnTwoInclusive(0, 100) + ', ' + getRandomNumberBtnTwoInclusive(0, 100) + ', ' + getRandomNumberBtnTwoInclusive(0, 100) + ')',
+            ];
+            var easing = easings[Math.floor(Math.random() * easings.length)];
+            return easing;
+        }
+
+        function getRandomNumberBtnTwoInclusive(min, max) {
+            if (Number.isInteger(min) && Number.isInteger(max))
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            return Math.random() * (max - min + 1) + min;
+        }
+
+        function getRandomNumber(min = 1) {
+            return Math.floor(Math.random() * Number.MAX_VALUE + min);
+        }
     </script>
 
     <script src="{{ asset('global_assets/js/plugins/animejs/anime.min.js') }} "></script>
