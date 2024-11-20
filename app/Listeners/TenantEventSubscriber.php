872452090e27ab0d16c121257e9607eaf5f8de16
rayusamBoy <rysmtulia07@gmail.com>
2024-11-20 18:12:38 +0300

    Fix Call to a member function routeNotificationFor() on null exception when executing database tenant-related commands

--- app/Listeners/TenantEventSubscriber.php
+++ app/Listeners/TenantEventSubscriber.php
@@ -60,114 +60,114 @@ class TenantEventSubscriber
     public function handleDeletingThemeDir(TenantDeletingIDCardsThemeDir $event): void
     {
         $event->data = ['msg' => 'Deleting tenant ID Cards theme dir...', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleDeletingStorageDir(TenantDeletingStorageDir $event): void
     {
         $event->data = ['msg' => 'Deleting tenant storage dir...', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleThemeDirDeleted(TenantIDCardsThemeDirDeleted $event): void
     {
         $event->data = ['msg' => 'Tenant ID Cards theme deleted.', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleStorageDirDeleted(TenantStorageDirDeleted $event): void
     {
         $event->data = ['msg' => 'Tenant storage dir deleted.', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleCreatingTenant(CreatingTenant $event): void
     {
         $event->data = ['msg' => 'Creating tenant...', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleTenantCreated(TenantCreated $event): void
     {
         $event->data = ['msg' => 'Tenant created.', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleDeletingTenant(DeletingTenant $event): void
     {
         $event->data = ['msg' => 'Deleting tenant...', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleTenantDeleted(TenantDeleted $event): void
     {
         $event->data = ['msg' => 'Tenant deleted.', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleDBDeleted(DatabaseDeleted $event): void
     {
         $event->data = ['msg' => 'Tenant database deleted.', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleCreatingDomain(CreatingDomain $event): void
     {
         $event->data = ['msg' => 'Creating tenant domain...', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleDomainCreated(DomainCreated $event): void
     {
         $event->data = ['msg' => 'Tenant domain created.', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleDeletingDomain(DeletingDomain $event): void
     {
         $event->data = ['msg' => 'Deleting tenant domain...', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleDomainDeleted(DomainDeleted $event): void
     {
         $event->data = ['msg' => 'Tenant domain deleted.', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleCreatingDB(CreatingDatabase $event): void
     {
         $event->data = ['msg' => 'Creating tenant database...', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleDBMigration(MigratingDatabase $event): void
     {
         $event->data = ['msg' => 'Migrating tenant database...', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleDBMigrated(DatabaseMigrated $event): void
     {
         $event->data = ['msg' => 'Tenant database migrated.', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleSeedingDB(SeedingDatabase $event): void
     {
         $event->data = ['msg' => 'Seeding tenant database...', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleDBSeeded(DatabaseSeeded $event): void
     {
         $event->data = ['msg' => 'Tenant database seeded.', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 
     public function handleDBCreated(DatabaseCreated $event): void
     {
         $event->data = ['msg' => 'Tenant database created.', 'url' => route('tenants.index')];
-        Notification::sendNow(auth()->user(), new SendTenantEvent($event));
+        Notification::sendNow(auth()->user() ?? [], new SendTenantEvent($event));
     }
 }
