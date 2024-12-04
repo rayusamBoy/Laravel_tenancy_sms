3c35fe3c755db8678a0d88806b69cef49a6cda6e
rayusamBoy <rysmtulia07@gmail.com>
2024-12-04 17:12:15 +0300

    Set 1 as the minimum value for the limit key

--- app/Http/Requests/QueryBuilderRequest.php
+++ app/Http/Requests/QueryBuilderRequest.php
@@ -30,7 +30,7 @@ class QueryBuilderRequest extends FormRequest
             'condition_two' => 'required_unless:where_two,none|string',
             'input' => 'required_unless:where,none|string',
             'input_two' => 'required_unless:where_two,none|string',
-            'limit' => ['nullable', 'integer', 'min:0', 'max:' . DB::table($this->input("from"))->count()],
+            'limit' => ['nullable', 'integer', 'min:1', 'max:' . DB::table($this->input("from"))->count()],
             'orderby_column' => ['required', 'string', new ExistsInTableColsNames],
             'orderby_direction' => 'required|string|in:asc,desc',
         ];
